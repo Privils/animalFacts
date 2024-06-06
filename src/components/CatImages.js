8c360fa9ce3d4979db144e8472886d0f46d199ad
Priviledge <privilledgeblack8@gmail.com>
2024-06-06 14:17:54 +0200

    #

--- src/components/CatImages.js
+++ src/components/CatImages.js
@@ -40,7 +40,18 @@ const CatImages = () => {
       
       <div className="dogImageContainer">
     {loading ? (
-        <p id="loading">Loading....</p>
+         <p className="loading">
+         <span>L</span>
+         <span>o</span>
+         <span>a</span>
+         <span>d</span>
+         <span>i</span>
+         <span>n</span>
+         <span>g</span>
+         <span>.</span>
+         <span>.</span>
+         <span>.</span>
+       </p>
       ) : (
 
         <>
